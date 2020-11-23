99f6f57d0647bdf1b5267d727968915e92897198
Steve Tong <stevetong53@yahoo.com>
2020-11-22 23:15:28 -0800

    All the coding is done

--- Develop/app.js
+++ Develop/app.js
@@ -1,35 +1,144 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeArr = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function addUser() {
+    if (employeeArr === 0) {
+        getInfo();
+    }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    else {
+        const question = {
+            type: "confirm",
+            message: "Would you like to add another employee?",
+            name: "addUser"
+        }
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+        inquirer.prompt(question).then(function(data) {
+            if(data.addAUser) {
+                getInfo();
+            }
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+            else {
+                try {
+                    if(!fs.existsSync(OUTPUT_DIR)) {
+                        fs.mkdirSync(OUTPUT_DIR);
+                    } 
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+                    const returnedHTML = render(employees);
+                    fs.writeFileSync(outputPath, returnedHTML);
+                } 
+                catch (err) {
+                    console.log(err);
+                }
+            }
+        });
+    }
+}
+
+function getInfo() {
+    const question = {
+        type: "list",
+        message: "What type of employee do you want to add?",
+        name: "type",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    };
+
+    inquirer.prompt(question).then(function(employeeType)
+    {
+        const questions = 
+        [
+            {
+                type: "input",
+                message: `What is the name of the ${employeeType.employeeChoice}?`,
+                name: "name"
+            },
+            {
+                type: "number",
+                message: `What is the id of the ${employeeType.employeeChoice}?`,
+                name: "id"
+            },
+            {
+                type: "input",
+                message: `What is the email of the ${employeeType.employeeChoice}?`,
+                name: "email",
+            }
+        ];
+
+        inquirer.prompt(questions).then(function(employeeInformation)
+        {
+            switch (employeeType.employeeChoice)
+            {
+                case "Manager" :
+                    const managerQuestion = [
+                        {
+                            type: "input", 
+                            message: "What is the office number of this manager?",
+                            name: "officeNumber" 
+                        }];
+
+                    inquirer.prompt(managerQuestion).then(function(managerInformation) {
+                        const addManager = new Manager(employeeInformation.name, employeeInformation.id, employeeInformation.email, managerInformation.officeNumber);
+                        
+                        employees.push(addManager);
+
+                        addUser();
+                    });
+                    break;
+                    
+                case "Engineer" :
+                    const engineerQuestion = [
+                        {
+                            type: "input", 
+                            message: "What is their GitHub username?",
+                            name: "github" 
+                        }];
+
+                    inquirer.prompt(engineerQuestion).then(function(engineerInformation) {
+                        const addEngineer = new Engineer(employeeInformation.name, employeeInformation.id, employeeInformation.email, engineerInformation.github);
+                        
+                        employees.push(addEngineer);
+
+                        addUser();
+                    });
+                    break;
+
+                case "Intern" :
+                    const internQuestion = [
+                        {
+                            type: "input", 
+                            message: "What school are they attending?",
+                            name: "school"
+                        }];
+
+                    inquirer.prompt(internQuestion).then(function(internInformation) {
+                        const addIntern = new Intern(employeeInformation.name, employeeInformation.id, employeeInformation.email, internInformation.school);
+                        
+                        employees.push(addIntern);
+
+                        addUser();
+                    });
+                    break;
+                default:
+                    break;
+                    
+            }
+        });
+    });
+}
+
